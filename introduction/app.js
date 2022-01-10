9dfc78446c364cd2e693de1e514f5d739529acb1
idn-fahmifn <fahmifn.idn@gmail.com>
2022-01-10 23:58:39 +0700

    test

---
+++ introduction/app.js
@@ -0,0 +1,10 @@
+console.log('Hello World'); // menampilkan console hello world (string)
+console.log(123); // menampilkan console (int)
+console.log(true); // menampilkan console (boolean)
+
+var first = 'first variable';
+console.log(first) //memanggil class first
+
+console.log([1,2,3,4,5]) //menampilkan array
+console.log({a:1, b:2, c:3, d:4, e:5}) //menampilkan object
+console.table({a:1, b:2, c:3, d:4, e:5}) //menampilkan table
---
+++ variable/app.js
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+
+// var name = 'John Doe'; //variable awal
+// console.log(name);
+
+// name = 'Fahmi Nuradi';
+// console.log(name);
+
+// init var
+// var first;
+// console.log(first);
+
+// penulisan variable
+// var variable;
+// var _variable;
+// var vari_able;
+// var $variable;
+// var vari$able;
+// var variable123;
+// var vari123able123;
+
+
+// multilevel words
+// var firstname; //lowercase
+// var firstName; //camel case
+// var FirstName; //pascal case
+// var first_case; //snake case
+
+/**
+ * multiline comments
+ */
+
+
+// let
+
+// let firstname = 'fahmi nuradi';
+// firstname = 'michael siregar'
+
+// console.log(firstname);
+
+
+// const
+
+
