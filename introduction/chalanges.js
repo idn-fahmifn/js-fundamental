290c1bab2c929f98bb9c6c11b8efac151076f50c
idn-fahmifn <fahmifn.idn@gmail.com>
2022-01-26 14:13:44 +0700

    chalenges

---
+++ introduction/chalanges.js
@@ -0,0 +1,50 @@
+/**
+ * ================= Challenges ========================================================================
+ * 
+ * 
+ *
+ * Buatlah cek tipe data dengan output :
+ * 
+ * ini tipe data string
+ * ini tipe data boolean
+ * ini tipe data undifined 
+ * ini tipe data object
+ * 
+ * buatlah looping dari kata berikut dan tentukan jumlah karakternya nya  : 
+ * 
+ * - Hello
+ * - Assalamualaikum
+ * - Selamat Datang
+ * - semangat!!
+ * 
+ * buatlah object dengan nama siswa dari properti : 
+ * 
+ * nama depan : almerio
+ * nama belakang : setiawan
+ * kelas : 10
+ * kompetensi keahlian : Rekayasa Perangkat Lunak 
+ * alamat : -   jalan : jalan raya dayeuh
+ *              kecamatan: jonggol
+ *              kabupaten : bogor
+ * sekolah : IDN Boarding School
+ * nilai :  - diniyah : B+
+ *          - IT : A
+ *            bahasa inggris : B
+ * 
+ * buatlah output dengan memanggil : 
+ * 
+ * data siswa:
+ * nama lengkap : almerio setiawan
+ * kelas : 10
+ * kompetensi keahlian : rekayasa perangkat lunak
+ * asal kota : bogor
+ * nilai : diniyah (B+), IT (A), Bahasa Inggris (B)
+ * 
+ * 
+ * let nama = john kelas = 10 jurusan = RPL 
+ * buatlah output menggunakan template literal
+ * hallo nama saya john, saya berasal dari kelas 10 jurusan RPL.
+ * 
+ * 
+ * =============================================================================================================
+ */
